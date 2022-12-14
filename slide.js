59f5f920abed5123c43332020f6264a665733b45
UltiMultiGamer <42481788+UltiMultiGamer@users.noreply.github.com>
2022-12-14 18:37:16 +0300

    fortnite
    can we get much higher

---
+++ slide.js
@@ -0,0 +1,16 @@
+var mySwiper = new Swiper('#myCarousel', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
