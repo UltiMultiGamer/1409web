f573e4728724f636f9b413329a449ad0b3e832a1
UltiMultiGamer <42481788+UltiMultiGamer@users.noreply.github.com>
2022-12-31 15:56:17 +0300

    CMS update 2
    this is gonna be terrible to host huh

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
 eleventyConfig.addPassthroughCopy('./src/mainstyle.css');
 eleventyConfig.addPassthroughCopy('./src/telegram.png');
 eleventyConfig.addPassthroughCopy('./src/VK.png');
+eleventyConfig.addPassthroughCopy('./src/webscript.js')
 
 
 
